cce93e428d32d60ae8412b5b98355c70e58d20d5
patdelfino <mpndelfino@tip.edu.ph>
2025-03-28 13:31:50 +0800

    Update Render configuration and routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
-    return 'This is a test route';
+Route::get('/', function () {
+    return view('welcome');
 });
